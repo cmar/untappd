d9853ff6d0a9d6fe2c7a4fa17c5b12e0c4ddcc52
Darby Frey <darby@electricshoes.com>
2012-12-17 12:34:21 -0600

    Updated examples and docs, cleaned up some code

--- lib/untappd/beer.rb
+++ lib/untappd/beer.rb
@@ -1,10 +1,10 @@
 module Untappd
-
   class Beer < Base
-
-    # since (optional) - The numeric ID of the last recent check-in. This provided to you in the next_query attribute.
-    # offset (optional) - The offset that you like the dataset to begin with.
-    # Each set returns 25 max records, so you can use that paginate the feed.
+    # options:
+    # * min_id (int, optional) - The numeric ID of the most recent check-in. This is provided to
+    # you in the next_query attribute.
+    # * max_id (int, optional) - The checkin ID that you want the results to start with
+    # * limit (int, optional) - The number of results to return, maximum of 50, default is 25
     def self.feed(beer_id, options={})
       options.merge!(auth_options)
       response = response_to_mash get("/beer/checkins/#{beer_id}", :query => options)
@@ -15,27 +15,18 @@ module Untappd
       response_to_mash get("/beer/info/#{beer_id}", :query => options)
     end
 
-    # offset (optional) - The offset that you like the dataset to
-    # begin with. Each set returns 25 max records, so you can use that paginate the results.
-    # sort (optional): "count" or "name" (default) - This can let you choose if you
-    # want the results to be returned in Alphabetical order (name) or by
-    # checkin count (count). By default the search returns all values in Alphabetical order.
+    # options:
+    # * sort (optional): "count" or "name" (default) - This can let you choose if you want the
+    # results to be returned in Alphabetical order (name) or by checkin count (count). By default
+    # the search returns all values in Alphabetical order.
     def self.search(q, options={})
       options.merge!(auth_options)
       options.merge!({ :q => q })
       response_to_mash get("/search/beer/", :query => options)
     end
 
-    # type (optional) - 4 options: "all", "macro", "micro", "local". "All" is set to default.
-    # limit (optional) - The number of records that you will return (max 10)
-    # age (optional) - 3 options: "daily", "weekly", "monthly".
-    # geolat (optional) - The numeric Latitude to filter the public feed. This is required for local "type".
-    # geolng (optional) - The numeric Longitude to filter the public feed. This is required for local "type".
-    def self.trending(options={})
-      options.merge!(auth_options)
-      response_to_mash get("/beer/trending", :query => options)
+    def self.trending
+      response_to_mash get("/beer/trending", :query => auth_options)
     end
-
   end
-
 end
--- lib/untappd/checkin.rb
+++ lib/untappd/checkin.rb
@@ -1,30 +1,33 @@
 module Untappd
-
   class Checkin < Base
-
-    def self.info(checkin_id, options={})
-      options.merge!(auth_options)
-      response_to_mash get("/checkin/view/#{checkin_id}", :query => options)
+    def self.info(checkin_id)
+      response_to_mash get("/checkin/view/#{checkin_id}", :query => auth_options)
     end
 
-    # since (optional) - The numeric ID of the last recent check-in. This provided to you in the next_query attribute.
-    # geolat (optional) - The numeric Latitude to filter the public feed.
-    # geolng (optional) - The numeric Longitude to filter the public feed.
-    # offset (optional) - The offset that you like the dataset to begin with.
-    #    Each set returns 25 max records, so you can use that paginate the feed.
+    # options:
+    # * since (int, optional) - The numeric ID of the most recent check-in.
+    # * lng (float, optional) - The numeric Latitude to filter the public feed.
+    # * lat (float, optional) - The numeric Longitude to filter the public feed.
+    # * radius (int, optional) - The max radius you would like the check-ins to start within
+    # * max_id (int, optional) - The checkin ID that you want the results to start with
+    # * limit (int, optional) - The number of results to return, max of 50, default is 25
     def self.feed(options={})
       options.merge!(auth_options)
       response_to_mash get("/thepub", :query => options)
     end
 
-    # foursquare_id (optional) - The MD5 hash ID of the Venue you want to attach
-    #        the beer checkin. This HAS TO BE the MD5 non-numeric hash from the foursquare v2. Older numeric id will not be accepted.
-    # user_lat (optional) - The numeric Latitude of the user. This is required if you add a location.
-    # user_lng (optional) - The numeric Longitude of the user. This is required if you add a location.
-    # shout (optional) - The text you would like to include as a comment of the checkin. Max of 140 characters.
-    # facebook (optional) - Default = "off", Pass "on" to post to facebook
-    # twitter (optional) - Default = "off", Pass "on" to post to twitter
-    # foursquare (optional) - Default = "off", Pass "on" to checkin on foursquare
+    # options:
+    # * foursquare_id (optional) - The MD5 hash ID of the Venue you want to attach the beer
+    # checkin. This HAS TO BE the MD5 non-numeric hash from the foursquare v2.
+    # * geolat (optional) - The numeric Latitude of the user. This is required if you add a location.
+    # * geolng (optional) - The numeric Longitude of the user. This is required if you add a location.
+    # * shout (optional) - The text you would like to include as a comment of the checkin.
+    # Max of 140 characters.
+    # * rating (optional) - The rating score you would like to add for the beer. This can only
+    # be 1 to 5 and whole numbers (no 4.2)
+    # * facebook (optional) - Default = "off", Pass "on" to post to facebook
+    # * twitter (optional) - Default = "off", Pass "on" to post to twitter
+    # * foursquare (optional) - Default = "off", Pass "on" to checkin on foursquare
     def self.create(access_token, gmt_offset, timezone, beer_id, options={})
       options.merge!({
         :gmt_offset   => gmt_offset,
@@ -32,13 +35,17 @@ module Untappd
         :bid          => beer_id
       })
 
-      response_to_mash post("/checkin/add", :query => {:access_token => access_token}, :body => options)
+      response_to_mash post("/checkin/add",
+        :query => {:access_token => access_token},
+        :body => options
+      )
     end
 
     def self.add_comment(access_token, checkin_id, comment)
-      query_params = { :access_token => access_token }
-      body_params = { :comment => comment }
-      response_to_mash post("/checkin/addcomment/#{checkin_id}", :query => query_params, :body => body_params)
+      response_to_mash post("/checkin/addcomment/#{checkin_id}",
+        :query => { :access_token => access_token },
+        :body => { :comment => comment }
+      )
     end
 
     def self.remove_comment(access_token, comment_id)
